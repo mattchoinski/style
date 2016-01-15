4e0ad42abb5d12b8c567e00fe6801ce1b76d0548
Matthew Choinski <hello@choinski.net>
2016-01-15 09:43:29 -0500

    Updated the grunt tasks by adding 'grunt watch'.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,33 @@
 module.exports = function(grunt) {
 
-	grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+	grunt.initConfig(
+		{
+			pkg: grunt.file.readJSON("package.json"),
 
-		cssmin: {
-			css: {
-				files: [
-					{
-						src: 'screen.css', 
-						dest: 'screen.min.css'
-					},
-					{
-						src: 'screen_ie.css', 
-						dest: 'screen_ie.min.css'
-					}
-				]
+			cssmin: {
+				css: {
+					files: [
+						{
+							src: "screen.css",
+							dest: "screen.min.css"
+						},
+						{
+							src: "screen_ie.css",
+							dest: "screen_ie.min.css"
+						}
+					]
+				}
+			},
+			watch: {
+				files: ["*.css"],
+				tasks: ["cssmin"]
 			}
 		}
+	);
 
-    });
+	grunt.loadNpmTasks("grunt-contrib-cssmin");
+	grunt.loadNpmTasks("grunt-contrib-watch");
 
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.registerTask('default', ['cssmin']);
+	grunt.registerTask("default", ["cssmin"]);
 
 };
